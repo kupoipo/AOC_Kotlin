6954cae2c66766df344a7de9b1ea9499cbc44747
kupoi <>
2023-12-04 12:00:15 +0100

    2023 - D4 non opti

--- src/main/kotlin/_2023/d4/Day4.kt
+++ src/main/kotlin/_2023/d4/Day4.kt
@@ -1,19 +1,55 @@
 package _2023.d4
 
 import util.Day
+import util.allInts
+import util.listOfMatch
 import util.readFullText
+import kotlin.math.pow
 import kotlin.system.measureTimeMillis
+
+class Card(val card : List<Int>, val winningValue : List<Int> ) {
+    fun nbNumberWinning() : Int = card.count { winningValue.contains(it) }
+}
 class Day4(override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+    val cards = mutableListOf<Card>()
+
+    init {
+        input.split("\n").forEachIndexed{ index, line ->
+            val groups = Regex("""(\d+ *)+(\||$)""").listOfMatch(line)
+            val tas = groups.map { it.allInts() }
+            cards.add(Card(tas.first(), tas.last()))
+        }
     }
+    override fun solve1(): Long = cards.sumOf {
+        val nbWinning = it.nbNumberWinning()
+        if (nbWinning != 0)
+            2.0.pow((it.nbNumberWinning() - 1).toDouble())
+        else
+            0.0
+    }.toLong()
+
+    fun sumOfCard(i : Int) : Int {
+        var total = 0
+        val card = cards[i]
+        val nbWinning = card.nbNumberWinning()
+        total += nbWinning
+
+        for (j in i+1..i+total) {
+            if (j <= cards.size)
+                total += sumOfCard(j)
+        }
+
+        return total
+    }
+
     override fun solve2(): Long {
-        return -1
+        return cards.indices.sumOf { 1 + sumOfCard(it).toLong() }
     }
 }
 
 fun main() {
-    val day = Day4(readFullText("_2023/d4/input"))
+    //var day = Day4(readFullText("_2023/d4/test"))
+    var day = Day4(readFullText("_2023/d4/input"))
 
     val t1 = measureTimeMillis { println("Part 1 : " + day.solve1()) }
     println("Temps partie 1 : {$t1}")
@@ -24,7 +60,7 @@ fun main() {
     println()
     println()
 
-    val dayTest = Day4(readFullText("_2023/d4/test"))
+    var dayTest = Day4(readFullText("_2023/d4/test"))
     val t1Test = measureTimeMillis { println("TEST - Part 1 : " + dayTest.solve1()) }
     println("Temps partie 1 : {$t1Test}")
 
