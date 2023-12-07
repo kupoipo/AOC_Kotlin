ab3bba3f190dc0de96934e0c9d773d4309fa662e
Yohann <yohann.jacquier@unicaen.fr>
2023-12-07 10:19:01 +0100

    2023 - Day 7

--- src/main/kotlin/_2023/d7/Day7.kt
+++ src/main/kotlin/_2023/d7/Day7.kt
@@ -2,14 +2,91 @@ package _2023.d7
 
 import util.Day
 import util.readFullText
+import java.util.PriorityQueue
 import kotlin.system.measureTimeMillis
-class Day7(override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+const val CARDS = "23456789TJQKA"
+const val CARDS_JOKER = "J23456789TQKA"
+
+class Hand(val cards: String, val bid: Int, val strength: Int, var joker: Boolean) : Comparable<Hand> {
+    companion object {
+        fun handFromLine(input: String, joker: Boolean = false): Hand {
+            input.split(" ").let { (cards, bid) ->
+                var map = CARDS.associateWith { c ->  cards.count { it == c } }
+
+                if (joker) {
+                    val nbJoker = map['J']
+
+                    if (nbJoker != 0) {
+                        map = map.filter { it.key != 'J' }.toMutableMap()
+
+                        val max = map.maxOf { it.value }
+
+                        for (k in map.keys) {
+                            if (map[k] == max) {
+                                map[k] = map[k]!! + nbJoker!!
+                                break
+                            }
+                        }
+                    }
+                }
+
+                when (map.maxOf { it.value }) {
+                    5 -> return Hand(cards, bid.toInt(), 7, joker)
+                    4 -> return Hand(cards, bid.toInt(), 6, joker)
+                    3 -> {
+                        if (map.containsValue(2))
+                            return Hand(cards, bid.toInt(), 5, joker)
+                        return Hand(cards, bid.toInt(), 4, joker)
+                    }
+
+                    2 -> {
+                        if (map.count { it.value == 2 } == 2)
+                            return Hand(cards, bid.toInt(), 3, joker)
+                        return Hand(cards, bid.toInt(), 2, joker)
+                    }
+
+                    else -> return Hand(cards, bid.toInt(), 1, joker)
+                }
+            }
+        }
     }
-    override fun solve2(): Long {
-        return -1
+
+    override fun compareTo(other: Hand): Int {
+        val powerCards = if (joker) CARDS_JOKER else CARDS
+
+        if (strength != other.strength) return strength.compareTo(other.strength)
+        else {
+            for (i in cards.indices) {
+                if (cards[i] != other.cards[i])
+                    return powerCards.indexOf(cards[i]).compareTo(powerCards.indexOf(other.cards[i]))
+
+            }
+        }
+
+        return 0
     }
+
+    override fun toString(): String {
+        return "$cards - $bid - $strength"
+    }
+}
+
+class Day7(override val input: String) : Day<Long>(input) {
+    val hands = mutableListOf<Hand>()
+    val handsJoker = mutableListOf<Hand>()
+
+    init {
+        input.split("\n").forEach {
+            hands.add(Hand.handFromLine(it))
+            handsJoker.add(Hand.handFromLine(it, true))
+        }
+
+        hands.sort()
+        handsJoker.sort()
+    }
+    override fun solve1(): Long = hands.mapIndexed { index, hand -> (index + 1) * hand.bid }.sum().toLong()
+    override fun solve2(): Long = handsJoker.mapIndexed { index, hand -> (index + 1) * hand.bid }.sum().toLong()
 }
 
 fun main() {
