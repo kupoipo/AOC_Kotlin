7f29400a0b9732928a651cd7b76f80241c13f367
Drakib <Drakib>
2024-12-01 13:17:31 +0100

    Jour 1 - 2024

--- src/main/kotlin/_2024/d1/Day1.kt
+++ src/main/kotlin/_2024/d1/Day1.kt
@@ -1,16 +1,20 @@
-
 package _2024.d1
 
 import util.Day
+import util.allLong
 import util.readFullText
+import kotlin.math.abs
 import kotlin.system.measureNanoTime
-class Day1(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
-    }
-    override fun solve2(): Long {
-        return -1
-    }
+
+class Day1(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    private val pairs = input.split("\n").map { line -> line.allLong() }
+
+    private val first = pairs.map { it.first() }
+    private val second = pairs.map { it.last() }
+
+    override fun solve1(): Long = first.indices.sumOf { i -> abs(first[i] - second[i]) }
+
+    override fun solve2(): Long = first.sumOf { s -> second.count { it == s } * s }
 }
 
 fun main() {
