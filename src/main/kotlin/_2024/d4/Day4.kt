3f46eaa9dfc07b2aa29ccdfc73d22e2ef1673378
yohann.jacquier@unicaen.fr <jacquie201@C302L-S21P01.png.unicaen.fr>
2024-12-04 10:24:37 +0100

    Day 4 - 2024

--- src/main/kotlin/_2024/d4/Day4.kt
+++ src/main/kotlin/_2024/d4/Day4.kt
@@ -1,16 +1,44 @@
-
 package _2024.d4
 
-import util.Day
-import util.readFullText
+import util.*
 import kotlin.system.measureNanoTime
-class Day4(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+class Day4(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    private val map = matrixFromString(input, ' ') { it }
+
+    private fun isWord(p: Point, dir: Point, word: String): Boolean = word.indices.all {
+        val currentPosition = p + (dir * it)
+        currentPosition.inMap(map) && map[currentPosition] == word[it]
     }
-    override fun solve2(): Long {
-        return -1
+
+    /**
+     * For every position of [word] first letter, we ensure that the n-th position in a given direction is in the matrix
+     * and match the n-th letter of [word].
+     *
+     * @param word
+     * @return A list of (position, direction)
+     */
+    private fun getPlaces(word: String): List<Pair<Point, Point>> = buildList {
+        map.points().filter { map[it] == word.first() }.forEach { position ->
+            (-1..1).flatMap { y -> (-1..1).map { x -> Point(x, y) } }.forEach { direction ->
+                if (isWord(position, direction, word)) add(position to direction)
+            }
+        }
     }
+
+    private fun nbXMAS() =
+        map.points().sumOf { p ->
+            val pos = listOf(Point(0, 0), Point(2, 0), Point(1, 1), Point(0, 2), Point(2, 2))
+            listOf("SSAMM", "MMASS", "MSAMS", "SMASM").count { word ->
+                (word.indices).all {
+                    val position = pos[it] + p
+                    position.inMap(map) && word[it] == map[position]
+                }
+            }
+        }
+
+    override fun solve1(): Long = getPlaces("XMAS").size.toLong()
+    override fun solve2(): Long = nbXMAS().toLong()
 }
 
 fun main() {
