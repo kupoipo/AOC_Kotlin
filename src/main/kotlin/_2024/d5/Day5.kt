d5685ea3523df292c7c9637ea63828e8a9534d1d
yohann.jacquier@unicaen.fr <jacquie201@C302L-S21P01.png.unicaen.fr>
2024-12-05 10:13:03 +0100

    Day 5 - 2024

--- src/main/kotlin/_2024/d5/Day5.kt
+++ src/main/kotlin/_2024/d5/Day5.kt
@@ -1,16 +1,53 @@
-
 package _2024.d5
 
-import util.Day
-import util.readFullText
+import util.*
 import kotlin.system.measureNanoTime
-class Day5(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+class Day5(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    private val rules = input.split("\n\n").first().split("\n").map { it.allLong() }.let { pairs ->
+        mutableMapOf<Long, MutableSet<Long>>().apply {
+            for (pair in pairs) {
+                this.getOrPut(pair.first()) { mutableSetOf() }.add(pair.last())
+            }
+        }
     }
-    override fun solve2(): Long {
-        return -1
+
+    private val updates = input.split("\n\n").last().split("\n").map { it.allLong() }
+
+    private fun isCorrect(update: List<Long>) = update.indices.all { i ->
+        val number = update[update.lastIndex - i]
+        val before = update.take(update.lastIndex - i)
+
+        before.all { n -> rules[number] == null || n !in rules[number]!! }
     }
+
+    private fun middle(update: List<Long>) = update[update.size / 2]
+
+    private fun getPermute(update: List<Long>): Pair<Int, Int> {
+        for (i in update.indices) {
+            for (j in (i + 1 until update.size)) {
+                val n = update[i]
+                val n2 = update[j]
+                if (rules[n2] != null && n in rules[n2]!!) {
+                    return i to j
+                }
+            }
+        }
+        throw Exception("Shouldn't appear")
+    }
+
+    private fun correct(update: MutableList<Long>): List<Long> {
+        while (!isCorrect(update)) {
+            getPermute(update).let {
+                update.swap(it.first, it.second)
+            }
+        }
+
+        return update
+    }
+
+    override fun solve1(): Long = updates.filter(::isCorrect).sumOf(::middle)
+    override fun solve2(): Long = updates.filterNot(::isCorrect).map(::correct).sumOf(::middle)
 }
 
 fun main() {
