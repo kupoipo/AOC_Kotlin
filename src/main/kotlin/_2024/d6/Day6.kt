5ac77c9e3151e8bd064fd3edd5e5369aa6056a78
JACQUIER Yohann <yohann.jacquier@unicaen.fr>
2024-12-06 14:50:31 +0100

    Day 6 - 2024 (Brut)

--- src/main/kotlin/_2024/d6/Day6.kt
+++ src/main/kotlin/_2024/d6/Day6.kt
@@ -1,16 +1,69 @@
-
 package _2024.d6
 
-import util.Day
-import util.readFullText
+import util.*
 import kotlin.system.measureNanoTime
-class Day6(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+
+data class PositionDirection(val p: Point, val dir: Direction)
+
+class Day6(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    val map = matrixFromString(input, '.') { it }
+    val start = map.pointOfFirst { it == '^' }
+
+    private fun places(origin: Point): Set<Point> {
+        var current = origin
+        var direction = Direction.UP
+        val visited = mutableSetOf<Point>()
+
+        while (current.inMap(map)) {
+            visited.add(current)
+            val nextPosition = current + direction
+            if (nextPosition.inMap(map) && map[nextPosition] == '#') {
+                direction = direction.right()
+            } else {
+                current = nextPosition
+            }
+        }
+
+        return visited
     }
-    override fun solve2(): Long {
-        return -1
+
+    fun isLoop(p: Point): Boolean {
+        if (map[p] == '#') return false
+
+        var isLoop = false
+        map[p] = '#'
+
+
+        var current = start
+        var direction = Direction.UP
+        val visited = mutableSetOf<PositionDirection>()
+
+        while (current.inMap(map)) {
+            val pd = PositionDirection(current, direction)
+
+            if (pd in visited) {
+                isLoop = true
+                break
+            }
+
+            visited.add(PositionDirection(current, direction))
+            val nextPosition = current + direction
+            if (nextPosition.inMap(map) && map[nextPosition] == '#') {
+                direction = direction.right()
+            } else {
+                current = nextPosition
+            }
+        }
+
+
+
+        map[p] = '.'
+        return isLoop
     }
+
+    override fun solve1(): Long = places(map.pointOfFirst { it == '^' }).size.toLong()
+    override fun solve2(): Long = map.points().count { isLoop(it) }.toLong()
 }
 
 fun main() {
