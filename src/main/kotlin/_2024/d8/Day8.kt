488ac9ce84b0e220a0a1d8eb3335ab556994cd43
Drakib <Drakib>
2024-12-08 12:58:38 +0100

    Day 8 - 2024

--- src/main/kotlin/_2024/d8/Day8.kt
+++ src/main/kotlin/_2024/d8/Day8.kt
@@ -1,16 +1,41 @@
-
 package _2024.d8
 
-import util.Day
-import util.readFullText
+import util.*
 import kotlin.system.measureNanoTime
-class Day8(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+class Day8(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    private val map = matrixFromString(input, '.') { it }
+    private val antennas = buildMap<Char, MutableList<Point>> {
+        map.points().forEach { p ->
+            if (map[p] != '.') {
+                this.getOrPut(map[p]) { mutableListOf() }.add(p)
+            }
+        }
     }
-    override fun solve2(): Long {
-        return -1
-    }
+
+    override fun solve1(): Long = buildSet {
+        antennas.values.forEach { points ->
+            points.pairs().forEach { (p, p2) ->
+                addAll(listOf(p.symmetries(p2), p2.symmetries(p)).filter { it.inMap(map) })
+            }
+        }
+    }.size.toLong()
+
+    override fun solve2(): Long = buildSet {
+        antennas.values.forEach { points ->
+            points.pairs(true).forEach { (p, p2) ->
+                var current = p.symmetries(p2)
+                val delta = p2 - p
+
+                while (current.inMap(map)) {
+                    add(current)
+                    current += delta
+                }
+                addAll(points)
+            }
+        }
+    }.size.toLong()
+
 }
 
 fun main() {
@@ -31,5 +56,4 @@ fun main() {
             break
         }
     }
-
 }
