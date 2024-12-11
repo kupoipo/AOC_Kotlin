4b0a0bf217b06c11d8689584e23a0bd6bf0ae036
JACQUIER Yohann <yohann.jacquier@unicaen.fr>
2024-12-11 12:55:23 +0100

    Day 11 - 2024

--- src/main/kotlin/_2024/d11/Day11.kt
+++ src/main/kotlin/_2024/d11/Day11.kt
@@ -1,16 +1,32 @@
-
 package _2024.d11
 
 import util.Day
 import util.readFullText
+import java.math.BigInteger
 import kotlin.system.measureNanoTime
-class Day11(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
-        return -1
+
+class Day11(private val isTest: Boolean, override val input: String) : Day<BigInteger>(input) {
+    private var stones = input.split(" ").map { BigInteger.valueOf(it.toLong()) }
+    private var cache = mutableMapOf<Pair<BigInteger, Int>, BigInteger>()
+
+    private fun solve(stone: BigInteger, blink: Int): BigInteger {
+        if (blink == 0) return BigInteger.ONE
+        return cache.getOrPut(stone to blink) {
+            if (stone == BigInteger.ZERO) solve(BigInteger.ONE, blink - 1)
+            else {
+                val length = stone.toString().length
+                if (length % 2 == 0) {
+                    solve(BigInteger.valueOf(stone.toString().take(length / 2).toLong()), blink - 1) +
+                            solve(BigInteger.valueOf(stone.toString().drop(length / 2).toLong()), blink - 1)
+                } else {
+                    solve(stone.times(BigInteger.valueOf(2024)), blink - 1)
+                }
+            }
+        }
     }
-    override fun solve2(): Long {
-        return -1
-    }
+
+    override fun solve1(): BigInteger = stones.map { solve(it, 25) }.sumOf { it }
+    override fun solve2(): BigInteger = stones.map { solve(it, 75) }.sumOf { it }
 }
 
 fun main() {
