94c7ef0fb51f8c2e40b6812da3c54e684a751bf3
Drakib <Drakib>
2025-12-03 14:19:22 +0100

    2025 - Day 3

--- src/main/kotlin/_2025/d3/Day3.kt
+++ src/main/kotlin/_2025/d3/Day3.kt
@@ -1,16 +1,52 @@
-
 package _2025.d3
 
+import _2021.d18.split
 import util.Day
 import util.readFullText
 import kotlin.system.measureNanoTime
-class Day3(private val isTest: Boolean, override val input : String) : Day<Long>(input) {
-    override fun solve1(): Long {
+
+class Day3(private val isTest: Boolean, override val input: String) : Day<Long>(input) {
+    private val banks = input.split('\n')
+    private val joltages = mutableMapOf<String, MutableList<MutableList<Int>>>()
+
+    init {
+        input.split("\n").forEach { bank ->
+            val positions = MutableList<MutableList<Int>>(10) { mutableListOf() }
+
+            bank.forEachIndexed { index, c -> positions[c.digitToInt()].add(index) }
+
+            joltages[bank] = positions
+        }
+    }
+
+    private fun biggestJoltageRecursive(
+        positions: MutableList<MutableList<Int>>,
+        currentIndex: Int,
+        currentNumber: String,
+        joltage: Int
+    ): Long {
+        if (currentNumber.length == joltage) return currentNumber.toLong()
+
+        for (number in 9 downTo 0) {
+            val currentPositions = positions[number]
+
+            for (position in currentPositions) {
+                if (position > currentIndex) {
+                    val res = biggestJoltageRecursive(positions, position, currentNumber + number.toString(), joltage)
+                    if (res != -1L) return res
+                }
+            }
+        }
+
         return -1
     }
-    override fun solve2(): Long {
-        return -1
-    }
+
+    private fun biggestJoltage(bank: String, joltage: Int): Long =
+        biggestJoltageRecursive(joltages[bank]!!, -1, "", joltage)
+
+    override fun solve1(): Long = banks.sumOf { biggestJoltage(it, 2) }
+
+    override fun solve2(): Long = banks.sumOf { biggestJoltage(it, 12) }
 }
 
 fun main() {
